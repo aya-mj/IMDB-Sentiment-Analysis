b663bce7619b5ec6707205833fc23e350afefb43
Aya Mhjb <ayamahjoub6@gmail.com>
2025-03-01 12:38:18 +0000

    adding comments

--- DL_NN_code.py
+++ DL_NN_code.py
@@ -18,24 +18,28 @@ print(df.head())
 print(df.info())
 
 stop_words = set(stopwords.words('english'))
+# preprocessing function 
 def clean_text(text):
-    text = text.lower()
+    text = text.lower() # Convert to lowercase
     text = re.sub('<.*?>', '', text)  # Remove HTML tags
     text = re.sub('http\S+|www\S+', '', text)  # Remove URLs
     text = re.sub('[^a-z\s]', '', text)
-    tokens = text.split()
+    tokens = text.split() 
     tokens = [word for word in tokens if word not in stop_words]  # Remove stopwords
 
     return ' '.join(tokens)
 
 df['cleaned review'] = df['review'].apply(clean_text)
 
+# TF-IDF : converting the data into suitable format for neural network
 vectorizer = TfidfVectorizer(max_features=5000)
 X = vectorizer.fit_transform(df['cleaned review']).toarray()
 y = df['sentiment'].map({'positive': 1, 'negative': 0})
 
+#trainning the model
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+#model building
 
 model = Sequential([
     Dense(256, activation = 'relu', input_shape=(5000,),kernel_regularizer=l2(0.01)),
@@ -49,6 +53,7 @@ model = Sequential([
 model.compile(optimizer=Adam(learning_rate=0.0005), loss='binary_crossentropy', metrics=['accuracy'])
 early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
 
+#model training
 history = model.fit(X_train, y_train, epochs=15, batch_size=64, validation_data=(X_test, y_test), callbacks=[early_stopping])
 test_loss, test_accuracy = model.evaluate(X_test, y_test)
 
@@ -65,6 +70,7 @@ print(f"Test Accuracy: {test_accuracy:.4f}")
 # plt.ylabel('Count')
 # plt.show()
 
+# ploting accuracy and loss over epochs
 plt.figure(figsize=(10, 6))
 plt.subplot(1, 2, 1)
 plt.plot(history.history['accuracy'], label='Training Accuracy')
